03ddd851ae90d09ca7b4729959f7befd1b6b657d
Emmanuel Di Pretoro <edipretoro@gmail.com>
2013-02-26 11:44:06 +0100

    * lib/Biblio/Record/USBC.pm: first increment to pass the tests against the 'language' attribute

--- lib/Biblio/Record/USBC.pm
+++ lib/Biblio/Record/USBC.pm
@@ -8,9 +8,17 @@ has 'weight' => (
     is => 'rw'
 );
 
+has 'language' => (
+    is => 'rw'
+);
+
 sub computed_weight {
     my ( $self ) = @_;
     return substr( $self->weight, length( $self->weight ) - 1, 1);
+}
+
+sub computed_language {
+    return 0;
 }
 
 1;
